8f1c65152500a67880268a03659fa679f5360b34
hanhankabayan <hanhan@kabayan.id>
2025-02-01 13:10:30 +0700

    fix: moota autoconfirm

--- moota_autoconfirm.py
+++ moota_autoconfirm.py
@@ -241,7 +241,7 @@ async def main():
                     "status": "PAID",
                     "payment.method": PaymentMethodData.TRANSFER.value,
                     "payment.paid_at": GetCurrentDateTime(),
-                    "payment.description": "Dikonfirmasi Oleh Moota",
+                    "payment.description": f"Pembayaran Tagihan Periode{DateIDFormatter(str(invoice.get('due_date')))} (By Moota)",
                     "payment.confirmed_at": GetCurrentDateTime(),
                     "payment.confirmed_by": "moota@gmail.com",
                 }
