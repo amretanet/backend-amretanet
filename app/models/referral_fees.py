836652b38447fd7196410f73993c010a6108b7ce
hanhankabayan <hanhan@kabayan.id>
2025-04-18 23:01:33 +0700

    add: request fee actions

--- app/models/referral_fees.py
+++ app/models/referral_fees.py
@@ -1,8 +1,23 @@
 from datetime import datetime
+from typing import Optional
 from pydantic import BaseModel
 from enum import Enum
 
 # responses
+ReferralFeeProjections = {
+    "name": "$user_data.name",
+    "email": "$user_data.email",
+    "phone_number": "$user_data.phone_number",
+    "referral": "$user_data.referral",
+    "saldo": "$user_data.saldo",
+    "id_user": "$user_data._id",
+    "created_at": 1,
+    "date": 1,
+    "nominal": 1,
+    "status": 1,
+    "description": 1,
+    "reason": 1,
+}
 ReferralFeeUserProjections = {
     "name": 1,
     "email": 1,
@@ -15,6 +30,12 @@ ReferralFeeUserProjections = {
 
 
 # schemas
+class ReferralFeeStatusData(str, Enum):
+    ACCEPTED = "ACCEPTED"
+    PENDING = "PENDING"
+    REJECTED = "REJECTED"
+
+
 class ReferralFeePayoffData(BaseModel):
     id_user: str
     date: datetime
@@ -23,7 +44,9 @@ class ReferralFeePayoffData(BaseModel):
     method: str
 
 
-class ReferralFeeStatusData(str, Enum):
-    ACCEPTED = "ACCEPTED"
-    PENDING = "PENDING"
-    REJECTED = "REJECTED"
+class ReferralFeeUpdateData(BaseModel):
+    status: Optional[ReferralFeeStatusData] = None
+    date: Optional[datetime] = None
+    nominal: Optional[int] = None
+    description: Optional[str] = None
+    method: Optional[str] = None
--- app/routes/v1/referral_fee_routes.py
+++ app/routes/v1/referral_fee_routes.py
@@ -15,7 +15,9 @@ from app.models.users import (
 )
 from app.models.referral_fees import (
     ReferralFeePayoffData,
+    ReferralFeeProjections,
     ReferralFeeStatusData,
+    ReferralFeeUpdateData,
     ReferralFeeUserProjections,
 )
 from app.modules.database import AsyncIOMotorClient, GetAmretaDatabase
@@ -24,8 +26,11 @@ from app.modules.generals import (
     ObjectIDValidator,
 )
 from app.modules.response_message import (
+    DATA_HAS_DELETED_MESSAGE,
     DATA_HAS_INSERTED_MESSAGE,
+    DATA_HAS_UPDATED_MESSAGE,
     FORBIDDEN_ACCESS_MESSAGE,
+    NOT_FOUND_MESSAGE,
     SYSTEM_ERROR_MESSAGE,
     OBJECT_ID_NOT_VALID_MESSAGE,
 )
@@ -35,6 +40,98 @@ from dotenv import load_dotenv
 load_dotenv()
 
 router = APIRouter(prefix="/referral-fee", tags=["Referral Fees"])
+
+
+@router.get("")
+async def get_referral_fees(
+    key: str = None,
+    status: ReferralFeeStatusData = None,
+    id_user: str = None,
+    page: int = 1,
+    items: int = 10,
+    current_user: UserData = Depends(GetCurrentUser),
+    db: AsyncIOMotorClient = Depends(GetAmretaDatabase),
+):
+    if current_user.role == UserRole.CUSTOMER:
+        raise HTTPException(
+            status_code=403, detail={"message": FORBIDDEN_ACCESS_MESSAGE}
+        )
+
+    query = {}
+    if key:
+        query = {
+            "$or": [
+                {"description": {"$regex": key, "$options": "i"}},
+                {"reason": {"$regex": key, "$options": "i"}},
+            ]
+        }
+    if status:
+        query["status"] = status.value
+    if id_user:
+        id_user = ObjectIDValidator(id_user)
+        if not id_user:
+            raise HTTPException(
+                status_code=400, detail={"message": OBJECT_ID_NOT_VALID_MESSAGE}
+            )
+
+        query["id_user"] = id_user
+
+    pipeline = [
+        {"$match": query},
+        {
+            "$addFields": {
+                "status_priority": {
+                    "$switch": {
+                        "branches": [
+                            {
+                                "case": {
+                                    "$eq": [
+                                        "$status",
+                                        ReferralFeeStatusData.PENDING.value,
+                                    ]
+                                },
+                                "then": 1,
+                            },
+                            {
+                                "case": {
+                                    "$eq": [
+                                        "$status",
+                                        ReferralFeeStatusData.ACCEPTED.value,
+                                    ]
+                                },
+                                "then": 2,
+                            },
+                        ],
+                        "default": 3,
+                    }
+                }
+            }
+        },
+        {"$sort": {"status_priority": 1, "created_at": -1, "updated_at": -1}},
+        {
+            "$lookup": {
+                "from": "users",
+                "localField": "id_user",
+                "foreignField": "_id",
+                "as": "user_data",
+            }
+        },
+        {"$unwind": "$user_data"},
+    ]
+    referral_fee_data, count = await GetManyData(
+        db.referral_fees,
+        pipeline,
+        ReferralFeeProjections,
+        {"page": page, "items": items},
+    )
+    pagination_info: Pagination = {"page": page, "items": items, "count": count}
+
+    return JSONResponse(
+        content={
+            "referral_fee_data": referral_fee_data,
+            "pagination_info": pagination_info,
+        }
+    )
 
 
 @router.get("/users")
@@ -146,3 +243,76 @@ async def pay_off_referral_fee(
     await CreateOneData(db.expenditures, payload)
 
     return JSONResponse(content={"message": DATA_HAS_INSERTED_MESSAGE})
+
+
+@router.put("/update/{id}")
+async def update_referral_fee(
+    id: str,
+    data: ReferralFeeUpdateData = Body(..., embed=True),
+    current_user: UserData = Depends(GetCurrentUser),
+    db: AsyncIOMotorClient = Depends(GetAmretaDatabase),
+):
+    exist_referral_fees = await GetOneData(db.referral_fees, {"_id": ObjectId(id)})
+    if not exist_referral_fees:
+        raise HTTPException(status_code=400, detail={"message": NOT_FOUND_MESSAGE})
+
+    payload = data.dict(exclude_unset=True, exclude_none=True)
+    payload["updated_at"] = GetCurrentDateTime()
+
+    if "status" in payload and payload["status"] == ReferralFeeStatusData.ACCEPTED:
+        user_data = await GetOneData(
+            db.users, {"_id": ObjectId(exist_referral_fees["id_user"])}
+        )
+        if user_data.get("saldo", 0) < payload["nominal"]:
+            raise HTTPException(
+                status_code=400,
+                detail={
+                    "message": "Jumlah Saldo Tidak Mencukupi Untuk Melakukan Penarikan!"
+                },
+            )
+        payload["accepted_at"] = GetCurrentDateTime()
+        payload["accepted_by"] = ObjectId(current_user.id)
+    if "status" in payload and payload["status"] == ReferralFeeStatusData.REJECTED:
+        payload["rejected_at"] = GetCurrentDateTime()
+        payload["rejected_by"] = ObjectId(current_user.id)
+
+    result = await UpdateOneData(
+        db.referral_fees, {"_id": ObjectId(id)}, {"$set": payload}
+    )
+    if not result:
+        raise HTTPException(status_code=500, detail={"message": SYSTEM_ERROR_MESSAGE})
+
+    if "status" in payload and payload["status"] == ReferralFeeStatusData.ACCEPTED:
+        current_saldo = user_data["saldo"] - payload["nominal"]
+        await UpdateOneData(
+            db.users,
+            {"_id": ObjectId(exist_referral_fees["id_user"])},
+            {"$set": {"saldo": current_saldo}},
+        )
+        payload["category"] = "BONUS MITRA"
+        payload["id_referral_fee"] = ObjectId(id)
+        del payload["status"]
+        await CreateOneData(db.expenditures, payload)
+
+    return JSONResponse(content={"message": DATA_HAS_UPDATED_MESSAGE})
+
+
+@router.delete("/delete/{id}")
+async def delete_referral_fee(
+    id: str,
+    current_user: UserData = Depends(GetCurrentUser),
+    db: AsyncIOMotorClient = Depends(GetAmretaDatabase),
+):
+    if current_user.role == UserRole.CUSTOMER:
+        raise HTTPException(
+            status_code=403, detail={"message": FORBIDDEN_ACCESS_MESSAGE}
+        )
+    exist_referral_fee = await GetOneData(db.referral_fees, {"_id": ObjectId(id)})
+    if not exist_referral_fee:
+        raise HTTPException(status_code=404, detail={"message": NOT_FOUND_MESSAGE})
+
+    result = await DeleteOneData(db.referral_fees, {"_id": ObjectId(id)})
+    if not result.deleted_count:
+        raise HTTPException(status_code=500, detail={"message": SYSTEM_ERROR_MESSAGE})
+
+    return JSONResponse(content={"message": DATA_HAS_DELETED_MESSAGE})
